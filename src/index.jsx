5e7e2f70848e7f14ca29f629432f2a8ba76ee86e
juli20008 <mia.li.mba@gmail.com>
2023-06-25 23:01:51 -0400

    React Comp

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from "./components/main-view/main-view";
+import { MovieCard } from "../movie-card/movie-card";
+
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
